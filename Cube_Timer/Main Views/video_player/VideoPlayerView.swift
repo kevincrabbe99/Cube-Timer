876c5f5c2a0b0d84608dfc45d72e2f0f1c09f053
kevincrabbe99 <43616942+kevincrabbe99@users.noreply.github.com>
2021-04-14 01:42:17 -0500

    Created video player

--- Cube_Timer/Main Views/video_player/VideoPlayerView.swift
+++ Cube_Timer/Main Views/video_player/VideoPlayerView.swift
@@ -22,15 +22,49 @@ struct VideoPlayerView: View {
         ZStack {
             
             Color.black
-                .opacity(0.8)
+                .opacity(0.9)
             
-            ZStack {
-                VideoPlayer(player: self.player)
-                    .onAppear() {
-                        player.play()
+            VStack {
+                HStack {
+                    Text("Apr 14, 2020 at 1:29am")
+                        .font(Font.custom("Play-Bold", size: 14))
+                    
+                    ZStack {
+                        Color.init("green")
+                            .cornerRadius(3)
+                            .opacity(0.8)
+                        Text("55 seconds")
+                            .font(Font.custom("Play-Bold", size: 13))
                     }
+                    .frame(width: 90, height: 20)
+                    .padding(.leading, 10)
+                    
+                    Spacer()
+                    
+                    
+                    IconButton(icon: Image.init(systemName: "trash.fill"), bgColor: .init("mint_cream"), iconColor: .init("very_dark_black"))
+                        .padding(.trailing, 10)
+                    IconButton(icon: Image.init(systemName: "xmark"), bgColor: Color.init("red"), iconColor: Color.init("mint_cream"))
+                }
+                
+                ZStack {
+                    
+                    Color.init("very_dark_black")
+                        .cornerRadius(5)
+                        .opacity(0.8)
+                    
+                    VideoPlayer(player: self.player)
+                        .onAppear() {
+                            player.play()
+                        }
+                        .padding(.all, 5)
+                }
             }
-            
+            .padding(.top, 10)
+            .padding(.bottom, 25)
+            .padding(.leading, 70)
+            .padding(.trailing, 70)
+                
         }
         
     }
